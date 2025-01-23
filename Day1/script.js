f4c1788e7c8e4eba9dfe9d50adbf33f4df25c6bb
anoop-unq <padakantianoop7@gmail.com>
2025-01-23 18:37:04 +0530

    js

---
+++ Day1/script.js
@@ -0,0 +1,19 @@
+
+
+
+function change(){
+    let clicked = document.getElementById('clickme'); 
+    let same = document.getElementById('hello');
+    clicked.textContent = "Hello World !";
+    same.textContent = "Clicked !";
+    same.classList.add("pott");
+}
+
+
+function pro(){
+    let she = document.getElementById("he");
+    let blower = document.getElementById("blower");
+    alert("External Js");
+    she.textContent = "External Js !";
+    blower.textContent = "External Js !";
+}
